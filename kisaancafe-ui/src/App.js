bbb1f45d0281712bb3dd4c39cd50ce495ebd7fae
stuti raj <stuti.a.raj@accenture.com>
2023-12-06 13:35:37 +0530

    button header & footer added

--- kisaancafe-ui/src/App.js
+++ kisaancafe-ui/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { SharedButton } from './Components/shared components/SharedButton';
+import { ProductCont } from './Components/shared components/ProductCont';
+import Header from './Components/shared components/Header';
 
 function App() {
+  const OnClick =()=>{
+    console.log("clicked");
+  }
+  const OnEditClick =()=>{
+    console.log("clicked for edit");
+  }
+  const OnAddClick =()=>{
+    console.log("clicked for add");
+  }
+const ProductDetails =[{
+  Name:"Pesticides",
+  Weight:"1 kg",
+  MRP:"900",
+  Technical:"h2o"
+}]
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <SharedButton label="ADD PRODUCT" OnClick={OnClick}/> 
+    {ProductDetails.map((product)=>{
+      return <ProductCont Addlabel ="ADD" Editlabel ="EDIT"  OnAddClick={OnAddClick} OnEditClick={OnEditClick}
+      ProductDetails={product}/>
+    })}
+    
+    <Header />
+    </>
+   
+);
 }
 
 export default App;
